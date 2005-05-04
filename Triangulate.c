f15d2779ff35d649167786a04e44992ac69bb253
dwilkins42 <dwilkins42@localhost>
2005-05-04 07:23:02 +0000

    Use floating point version of abs() to avoid conversion errors

--- Triangulate.c
+++ Triangulate.c
@@ -422,8 +422,8 @@ double PTAreaOfTriangle( PTTriangle *t )
 	
 	A = ((t->v[2].x - t->v[0].x) * (t->v[1].y - t->v[0].y) - 
 	  	 (t->v[2].y - t->v[0].y) * (t->v[1].x - t->v[0].x))/2.0; 
-
-	return abs( A );	
+	// use fabs rather than abs as precision is lost with abs on 64bit platforms
+	return fabs( A );	
 }
 
 
