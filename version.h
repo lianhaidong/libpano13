3310a055695726af823b0f2481ffa6fe4667d480
jim0watters <jim0watters@localhost>
2005-11-25 07:07:34 +0000

    update to ver 2.8

--- version.h
+++ version.h
@@ -34,22 +34,22 @@
 #define PTVERSION_NAME_LONG "LongVersion"
 #endif
 #ifndef LONGVERSION
-#define LONGVERSION "2.7.0.14 , Copyright (c) 1998-2001, H. Dersch, der@fh-furtwangen.de"
+#define LONGVERSION "2.8.0 , Copyright (c) 1998-2001, H. Dersch, der@fh-furtwangen.de"
 #endif
 
 #ifndef PTVERSION_FILEVERSIONNUMBER
-#define PTVERSION_FILEVERSIONNUMBER 2,7,0,14
+#define PTVERSION_FILEVERSIONNUMBER 2,8,0,0
 #endif
 
 #ifndef PTVERSION_NAME_FILEVERSION
 #define PTVERSION_NAME_FILEVERSION "FileVersion"
 #endif
 #ifndef PTVERSION_FILEVERSION
-#define PTVERSION_FILEVERSION "2.7.0.14\0"
+#define PTVERSION_FILEVERSION "2.8.0\0"
 #endif
 
 #ifndef PROGRESS_VERSION
-#define PROGRESS_VERSION "2.7.0.14\0"
+#define PROGRESS_VERSION "2.8.0\0"
 #endif
 
 #ifndef PTVERSION_NAME_LEGALCOPYRIGHT 
@@ -63,5 +63,5 @@
 #define PTVERSION_NAME_COMMENT "Comments"
 #endif
 #ifndef PTVERSION_COMMENT
-#define PTVERSION_COMMENT "Antialiasing\0"
+#define PTVERSION_COMMENT "MinGW\0"
 #endif
