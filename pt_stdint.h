12251de415e5844b7f25f5c56fb1cf4079697376
dangelo <dangelo@localhost>
2006-05-21 18:29:00 +0000

    stdint is not provided by M$ Visual C.

---
+++ pt_stdint.h
@@ -0,0 +1,24 @@
+/* pt_stdint.h */
+
+#ifndef PT_STDINT__H
+#define PT_STDINT__H
+
+# ifdef _MSC_VER  /* Microsoft Visual C++ */
+typedef signed char             int8_t;
+typedef short int               int16_t;
+typedef int                     int32_t;
+typedef __int64                 int64_t;
+ 
+typedef unsigned char             uint8_t;
+typedef unsigned short int        uint16_t;
+typedef unsigned int              uint32_t;
+/* no uint64_t */
+
+#  define vsnprintf _vsnprintf
+#  define snprintf _snprintf
+# else
+#  include <stdint.h>
+# endif
+
+#endif
+
