7e2ccd1eb4eaa5be6105da27e1d249a1ac0c0c26
maxlyons <maxlyons@localhost>
2006-05-29 18:22:57 +0000

    changes fix various compilation problems:
    * Moved declaration of three functions from ptcommon.h to new ptttiff.h file.  * Updated includes in PTcommon.c and PTuncrop.c
    * Updated pano12.def and libpano12.def to  include the same, latest, exports

---
+++ pttiff.h
@@ -0,0 +1,39 @@
+/*
+ *  pttiff.h
+ *
+ * 
+ *  Copyright Helmut Dersch and Max Lyons
+ *  
+ *  May 2006
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  Author: Max Lyons
+ * 
+ */
+
+#ifndef __PTtiff_h__
+
+#define __PTtiff_h__
+
+#include <tiffio.h>
+
+void getCropInformationFromTiff(TIFF *tif, CropInfo *c);
+
+int TiffGetImageParameters(TIFF *tiffFile, pt_tiff_parms *tiffData);
+int TiffSetImageParameters(TIFF *tiffFile, pt_tiff_parms *tiffData);
+
+#endif
