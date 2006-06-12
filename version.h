543e4e7867a949ab72aed0b790ee72e913b4b943
brunopostle <brunopostle@localhost>
2006-06-12 21:05:45 +0000

    Increment version to 2.8.4

--- version.h
+++ version.h
@@ -27,7 +27,7 @@
 
 //version of preferences file, used to verify data
 #ifndef VERSION
-#define VERSION "2.8.3 "
+#define VERSION "2.8.4 "
 #endif
 
 #ifndef PTVERSION_NAME_LONG
@@ -38,7 +38,7 @@
 #endif
 
 #ifndef PTVERSION_FILEVERSIONNUMBER
-#define PTVERSION_FILEVERSIONNUMBER 2,8,3,0
+#define PTVERSION_FILEVERSIONNUMBER 2,8,4,0
 #endif
 
 #ifndef PTVERSION_NAME_FILEVERSION
