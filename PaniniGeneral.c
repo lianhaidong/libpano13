7fb0765e998159573572cd476e0d72cb41265774
tksharpless <tksharpless@localhost>
2010-01-18 02:14:32 +0000

    Changed PaniniGeneral license to BSD 3-clause

--- PaniniGeneral.c
+++ PaniniGeneral.c
@@ -1,20 +1,34 @@
 /* PaniniGeneral.c		15Jan2010 TKS
 
+Copyright (c) 2010, Thomas K Sharpless
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 This is the reference implementation of the General Pannini
 Projection, an elaboration of the basic Pannini projection 
 discovered by Bruno Postle and Thomas Sharpless in December 
 2008 in paintings by Gian Paolo Pannini (1691-1765).
-
-    (C) copyright 2010 Thomas K Sharpless
-Free license is hereby granted for noncommercial use
-under the terms of the GNU Lesser General Public License 
-version 2.1 as published by the Free Software Foundation 
-and appearing in the file LICENSE.LGPL included in the
-packaging of this file.  Please review the following 
-information to ensure the requirementsGeneral of the GNU Lesser 
-General Public License version 2.1 will be met: 
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-
 
 */
 #include "PaniniGeneral.h"
