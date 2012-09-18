c19d0854e4ecf64be9e4fd4b80c04d41dd51f021
tmodes <tmodes@localhost>
2012-09-18 19:10:42 +0200

    Update build system to compile on Windows again
    Added option to CMake build to allow building static or shared version on Windows

--- sys_compat_unix.c
+++ sys_compat_unix.c
@@ -1,4 +1,6 @@
-/*
+/* Panorama_Tools	-	Generate, Edit and Convert Panoramic Images
+   Copyright (C) 1998,1999 - Helmut Dersch  der@fh-furtwangen.de
+   
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
@@ -11,21 +13,22 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  
-*/
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/* these are functions that have very different implementions in different systems, so we
-   define them here and implemement them in sys_ansi.c or sys_win.c */
+/*------------------------------------------------------------*/
 
-#ifndef SYS_COMPAT_H
-#define SYS_COMPAT_H
+#include "sys_compat.h"
+#include <assert.h>
 
-#include <time.h>
+int panoTimeToStrWithTimeZone(char *sTime, int len, struct tm  *time) 
+{
+    assert(len >= 11);
+    return strftime(sTime, len, "%H%M%S%z", time);
 
-char *panoBasenameOfExecutable(void);
+}
 
-int panoTimeToStrWithTimeZone(char *sTime, int len, struct tm  *time) ;
-
-
-
-#endif 
+char *panoBasenameOfExecutable(void)
+{
+    extern char *__progname;
+    return __progname;
+}
